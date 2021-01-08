9255fb758b588e6b7f0cad7223a7271cba048680
AlexEastwood <76944389+AlexEastwood@users.noreply.github.com>
2021-01-08 17:58:44 +0000

    Balanced the teams around experienced/inexperienced

--- app.py
+++ app.py
@@ -31,21 +31,35 @@ def clean_data(players):
 def player_teams(player_list):
     num_players_team = int(len(player_list) / len(constants.TEAMS))
 
-    i = 0
+
+    experienced_on_team = [{"team": constants.TEAMS[0], "experienced": 0, "inexperienced": 0}, 
+                           {"team": constants.TEAMS[1], "experienced": 0, "inexperienced": 0}, 
+                           {"team": constants.TEAMS[2], "experienced": 0, "inexperienced": 0}]
+    
     j = 0
-    experienced = 0
     
     for player in player_list:
-        if i < num_players_team:
-            if experienced == num_players_team / 2:
-                player["team"] = constants.TEAMS[j + 1]
-            player["team"] = constants.TEAMS[j]
-            i += 1
+        if player["experience"] == True:
+            while True:
+                if experienced_on_team[j]["experienced"] < num_players_team / 2:
+                    player["team"] = constants.TEAMS[j]
+                    experienced_on_team[j]["experienced"] += 1
+                    j = 0
+                    break
+                else:
+                    j += 1
+                    continue
         else:
-            player["team"] = constants.TEAMS[j + 1]
-            i = 1
-            j += 1
-            
+            while True:
+                if experienced_on_team[j]["inexperienced"] < num_players_team / 2:
+                    player["team"] = constants.TEAMS[j]
+                    experienced_on_team[j]["inexperienced"] += 1
+                    j = 0 
+                    break
+                else:
+                    j += 1
+                    continue
+
     return player_list
 
 def team_stats(team):
@@ -54,15 +68,27 @@ def team_stats(team):
             team = input("Please choose A, B or C\n")
         team = team.upper()
 
-        print("Team: {}".format(list_of_teams[team]))
+        print("\n\nTeam: {}".format(list_of_teams[team]))
         print("Total Players: {}".format(int(len(list_of_players) / len(constants.TEAMS))))
         print("Players on Team:")
         players_one_string = []
+        experienced = 0
+        inexperienced = 0
+        
         for player in list_of_players:
             if player["team"] == list_of_teams[team]:
                 player_name = player["name"]
                 players_one_string.append(player_name)
+            
+                if player["experience"] == True:
+                    experienced += 1
+                else:
+                    inexperienced += 1
+                
         print(", ".join(players_one_string))
+        print("Experienced: {}".format(experienced))
+        print("Inexperienced: {}".format(inexperienced))
+        
         
         print("Press Enter to continue")
         input()
@@ -78,7 +104,7 @@ for player in list_of_players:
 
 
 while True:
-    print("BASKETBALL TEAM STATS TOOL")
+    print("\n\nBASKETBALL TEAM STATS TOOL")
     print("\n\n----MENU----\n\n")
     print("""Here are your choices:\nA) Display Team Stats\nB) Quit\n""")
 
